1d5cbc9f4e5875cdb88bceed2e711af030c05569
Dawid <you@example.com>
2020-05-31 21:04:25 +0200

    dodana funkcja odejmowania

--- kalkulator.py
+++ kalkulator.py
@@ -4,8 +4,13 @@ def get_info():
 def dodaj(a,b):
     wynik = a+b
     return wynik
+
+def odejmij(a,b):
+    return a-b
     
 a = int(input())
 b = int(input())
 get_info()
 dodaj(a,b)
+
+print('koniec programu')
